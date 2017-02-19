5de71ed43d2d19954707754e7019f3be918f6f8e
Bazlur Rahman Rokon <anm_brr@yahoo.com>
2017-02-20 02:22:20 +0600

    Refactor code

--- src/main/java/com/bazlur/crawler/MainApp.java
+++ src/main/java/com/bazlur/crawler/MainApp.java
@@ -8,10 +8,11 @@ public class MainApp {
 	public static void main(String[] args) {
 
 		String urlToCrawl = "http://www.bazlur.com/";
+		String baseDir = "tmp";
 
 		Crawler crawler = new Crawler(url -> url.startsWith(urlToCrawl));
 		crawler.addUrl(urlToCrawl);
-		crawler.addPageProcessors(new SaveOnDiskProcessor("tmp"));
+		crawler.addPageProcessors(new SaveOnDiskProcessor(baseDir));
 		crawler.crawl();
 	}
 }
